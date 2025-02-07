02d05e747a10d120872d1e85a4c8637255b53996
John <keokline2015@gmail.com>
2025-02-06 23:28:00 -0600

    Fresh start is currently working, also added tailwindcss

---
+++ FrontEnd/swtccopy/src/Components/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="font-bold underline">Hello World</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
