0226be32fc962dba3d5153b1ac3019b096aae63d
John <keokline2015@gmail.com>
2025-02-10 16:33:25 -0600

    maybe fixed tailwind?

--- FrontEnd/swtccopy/postcss.config.js
+++ FrontEnd/swtccopy/postcss.config.js
@@ -1,6 +1,5 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
 };
-export default config;
