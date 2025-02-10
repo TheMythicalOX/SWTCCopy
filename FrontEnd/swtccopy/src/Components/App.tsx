a771e0d4346cc0ad160ea693d092de6a82687e10
John <keokline2015@gmail.com>
2025-02-10 17:26:33 -0600

    Working Fresh start

---
+++ FrontEnd/swtccopy/src/Components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer.tsx";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="font-bold underline">Hello World</p>
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
