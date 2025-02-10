5d7bc7cb12abacf984c93b0a3223dc6c0d47aa8c
TheMythicalOX <keokline2015@gmail.com>
2025-02-10 17:17:29 +0000

    Added footer component

--- FrontEnd/swtccopy/src/Components/App.tsx
+++ FrontEnd/swtccopy/src/Components/App.tsx
@@ -1,8 +1,10 @@
+import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p className="font-bold underline">Hello World</p>
+        <Footer />
       </header>
     </div>
   );
---
+++ FrontEnd/swtccopy/src/Components/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Hello Footer</div>;
+};
+
+export default Footer;
