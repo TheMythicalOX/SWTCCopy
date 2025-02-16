8a3913b25a17f955bb3f459a8f38651aec6a17c9
John <keokline2015@gmail.com>
2025-02-16 10:23:12 -0600

    Campus in a good place

--- FrontEnd/swtccopy/src/Components/Campus.tsx
+++ FrontEnd/swtccopy/src/Components/Campus.tsx
@@ -5,16 +5,20 @@ const Campus = () => {
   return (
     <div className="camp bg-campus-bg">
       <img src={hero} alt="TMP" className="2col  object-contain" />
-      <a href="/#">Visit Campus</a>
-      <p>
-        Picture yourself as a Charger—see classrooms, shops, and labs; meet
-        students and faculty; discover resources available that will help you
-        achieve your goals. Join us for a campus tour and enjoy our beautiful
-        country-side location. Not sure where to start? Our staff is ready to
-        help you explore your options by talking about programs you might be
-        interested in or by taking a career assessment to see what program is
-        the best fit for you.
-      </p>
+      <div className="p-5">
+        <a className="underline font-semibold text-xl p-4 pl-0" href="/#">
+          Visit Campus
+        </a>
+        <p className="p-4 pl-0">
+          Picture yourself as a Charger—see classrooms, shops, and labs; meet
+          students and faculty; discover resources available that will help you
+          achieve your goals. Join us for a campus tour and enjoy our beautiful
+          country-side location. Not sure where to start? Our staff is ready to
+          help you explore your options by talking about programs you might be
+          interested in or by taking a career assessment to see what program is
+          the best fit for you.
+        </p>
+      </div>
       <div className="style-grapgic bg-amber-400 w-auto h-2"></div>
     </div>
   );
