0621732474114749ac390fcbf4cd7d574f6710eb
John <keokline2015@gmail.com>
2025-02-16 10:18:44 -0600

    Header in a good spot

--- FrontEnd/swtccopy/src/Components/Header.tsx
+++ FrontEnd/swtccopy/src/Components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
 
   return (
     <div className="Head bg-header-bg font-medium">
-      <a href="/#" className="w-2/6 inline-block pl-5">
+      <a href="/#" className="w-3/12 inline-block ml-6">
         <img src={swtclogo} alt="swtclogo" />
       </a>
       <div className="icons pl-1">
@@ -18,16 +18,19 @@ const Header = () => {
         <img src={tmp} alt="TMP" className={logoBasic} />
         <img src={tmp} alt="TMP" className={logoBasic} />
       </div>
-      <div className="search ">
-        <form className="">
-          <input className="" type="text" placeholder="Search..."></input>
-          <input
-            type="image"
-            alt="Submit Form"
-            className=""
-            src={searchIcon}
-          ></input>
-        </form>
+      <div className="block p-0.5">
+        <input
+          className="border-none inline-block h-8 leading-4 mr-0 ml-4 p-2 w-44"
+          type="text"
+          placeholder="Search..."
+        ></input>
+        <input
+          type="image"
+          src={searchIcon}
+          alt="Search"
+          onClick={() => {}}
+          className="align-bottom max-h-8 p-1.5 border-white bg-foot-text bg-none filter-none leading-normal"
+        />
       </div>
       <Menu />
     </div>
