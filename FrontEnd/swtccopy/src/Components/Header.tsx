0188fe22ba8b7118185121463d4c65a4d971c26e
TheMythicalOX <keokline2015@gmail.com>
2025-02-15 18:12:45 -0600

    middle of getting headder done

--- FrontEnd/swtccopy/src/Components/Header.tsx
+++ FrontEnd/swtccopy/src/Components/Header.tsx
@@ -2,28 +2,34 @@ import React from "react";
 import swtclogo from "../images/swtlogo_hz_white.png";
 import searchIcon from "../images/search.gif";
 import Menu from "./Menu";
+import tmp from "../images/tmp.svg";
 
 const Header = () => {
+  const logoBasic = "w-14 inline-block pl-5";
+
   return (
     <div className="Head bg-header-bg font-medium">
-      <a href="/#">
-        <img src={swtclogo} alt="swtclogo" className="w-2/5" />
+      <a href="/#" className="w-2/6 inline-block pl-5">
+        <img src={swtclogo} alt="swtclogo" />
       </a>
-      <Menu />
-      <div className="icons">
-        {/* <img src={image} alt="TMP" className="2col  object-contain" />
-        <img src={image} alt="TMP" className="2col  object-contain" />
-        <img src={image} alt="TMP" className="2col  object-contain" />
-        <img src={image} alt="TMP" className="2col  object-contain" /> */}
+      <div className="icons pl-1">
+        <img src={tmp} alt="TMP" className={logoBasic} />
+        <img src={tmp} alt="TMP" className={logoBasic} />
+        <img src={tmp} alt="TMP" className={logoBasic} />
+        <img src={tmp} alt="TMP" className={logoBasic} />
       </div>
-      <div className="search">
-        <form>
-          <input type="text" placeholder="Search..."></input>
-          <button type="submit" className="bg-neutral-300 w-7">
-            <img src={searchIcon} alt="Search Icon" className="p-1.5" />
-          </button>
+      <div className="search ">
+        <form className="">
+          <input className="" type="text" placeholder="Search..."></input>
+          <input
+            type="image"
+            alt="Submit Form"
+            className=""
+            src={searchIcon}
+          ></input>
         </form>
       </div>
+      <Menu />
     </div>
   );
 };
