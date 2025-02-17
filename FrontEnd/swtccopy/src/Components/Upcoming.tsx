ea5fd2153e6f19d70823ee7b8792d08132712faa
TheMythicalOX <keokline2015@gmail.com>
2025-02-17 10:07:39 -0600

    improvements on upcoming section but transitions not working properly

--- FrontEnd/swtccopy/src/Components/Upcoming.tsx
+++ FrontEnd/swtccopy/src/Components/Upcoming.tsx
@@ -8,14 +8,20 @@ const Upcoming = () => {
   return (
     <div className="test">
       <div className="topEvents text-campus-bg inline-flex w-full pt-5">
-        <img src={event} alt="Event Icon" className="w-9"></img>
-        <h1 className="font-semibold text-nowrap text-center m-auto px-3">
+        <img src={event} alt="Event Icon" className="w-9 ml-4"></img>
+        <h1 className="font-semibold text-xl text-nowrap text-center m-auto px-3">
           UPCOMING EVENTS
         </h1>
         <div className="h-1 w-full bg-campus-bg m-auto"></div>
       </div>
-      <div className="images grid grid-cols-3 gap-5 p-5">
-        <img src={first} alt="TMP" className="1col  object-contain" />
+      <div className="images grid grid-cols-3 gap-5 ml-0 max-h-96 ">
+        <div className="relative group items-center transition-all duration-1000">
+          <div className="absolute text-center top-1/2 w-full ease-in-out transition-all duration-1000  group-hover:bg-campus-bg group-hover:bottom-0 group-hover:top-auto ">
+            <h2 className="">FEBRUARY 26</h2>
+            <p>Manufacturing Exploration Event</p>
+          </div>
+          <img src={first} alt="TMP" className="1col  object-contain" />
+        </div>
         <img src={middle} alt="TMP" className="1col  object-contain" />
         <img src={last} alt="TMP" className="1col  object-contain" />
       </div>
--- FrontEnd/swtccopy/tailwind.config.js
+++ FrontEnd/swtccopy/tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         "header-bg": "#00498F",
         "campus-bg": "#002950",
         "quick-link-bg": "rgb(0, 41, 80)",
+        "event-title-bg": "rgba(0, 0, 0, .7)",
       },
     },
   },
